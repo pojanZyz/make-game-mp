7e203ea0c484279d62c9e618101b9fe9a2e337e7
pojanZyz <akunword0@gmail.com>
2025-05-14 08:21:37 +0700

    publish

---
+++ script.js
@@ -0,0 +1,20 @@
+// let randomNumber;
+
+// function startGame() {
+//   randomNumber = Math.floor(Math.random() * 10) + 1;
+//   document.getElementById("gameArea").style.display = "block";
+//   document.getElementById("resultMessage").textContent = "";
+// }
+
+// function checkGuess() {
+//   const userGuess = parseInt(document.getElementById("userGuess").value);
+//   const message = document.getElementById("resultMessage");
+
+//   if (userGuess === randomNumber) {
+//     message.textContent = "Selamat! Kamu berhasil menebak angkanya!";
+//     message.className = "text-success";
+//   } else {
+//     message.textContent = "Ups, coba lagi ya!";
+//     message.className = "text-danger";
+//   }
+// }
